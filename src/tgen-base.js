e9c786dbf18926650100fad5cc4a8dbd45a0dea1
TS <git@schalk.hu>
2020-09-22 21:44:08 +0200

    Updated package.json and package-lock.json

--- src/tgen-base.js
+++ src/tgen-base.js
@@ -11,7 +11,7 @@ var SeamlessTextureGenerator = (function() {
 
     return {
 
-        version: '1.2.3',
+        version: '1.2.4',
         defaults: {},
         effects: {},
         filters: [],
