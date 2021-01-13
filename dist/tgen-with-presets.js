a31b5df7617b691ad1f1ff221f5fb846479155fc
TS <git@schalk.hu>
2021-01-13 21:12:03 +0100

    Updated version

--- dist/tgen-with-presets.js
+++ dist/tgen-with-presets.js
@@ -11,7 +11,7 @@ var SeamlessTextureGenerator = (function() {
 
     return {
 
-        version: '1.3.1',
+        version: '1.3.2',
         defaults: {},
         effects: {},
         filters: [],
