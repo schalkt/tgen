16ee47ae660088fab3025428916639116a09f42e
TS <git@schalk.hu>
2022-06-30 12:40:16 +0200

    Updated colorbar, dots Updated packages.json and lock Fixed code duplication

---
+++ src/tgen-base-common.js
@@ -0,0 +1,106 @@
+/**
+ * tgen.js - the seamless texture generator
+ *
+ * https://github.com/schalkt/tgen/
+ * https://texture-generator.com/
+ *
+ * @copyright 2015-2022 Tamas Schalk
+ * @version 1.4.10
+ * @license MIT
+ *
+ */
+
+var SeamlessTextureGenerator = {
+  version : "1.4.10",
+  defaults: {},
+  effects: {},
+  prepare: {},
+  filters: [],
+  presets: {},
+  functions: [],
+  blends: {},
+  shapes: {},
+  colormaps: {},
+  config: {},
+
+  blendFlat: ["lighten", "screen", "opacity"],
+
+  blendSafe: [
+    "average",
+    "lighten",
+    "linearburn",
+    "linearlight",
+    "difference",
+    "difference-invert",
+    "screen",
+    "lineardodge",
+    "lineardodge-invert",
+    "opacity",
+    "exclusion",
+  ],
+
+  randomRGBA: [
+    [0, 255],
+    [0, 255],
+    [0, 255],
+    [64, 255],
+  ],
+
+  effect: function (name, defaults, func, prepare) {
+    this.defaults[name] = defaults;
+    this.effects[name] = func;
+    this.prepare[name] = prepare;
+  },
+
+  function: function (name, defaults, func) {
+    this.functions.push(name);
+    this.defaults[name] = defaults;
+    this.effects[name] = func;
+  },
+
+  filter: function (name, defaults, func) {
+    this.filters.push(name);
+    this.defaults[name] = defaults;
+    this.effects[name] = func;
+  },
+
+  preset: function (name, params) {
+    params.name = name;
+    this.presets[name] = params;
+  },
+
+  event: function (when, name, func) {
+    if (this.events[when] == undefined) {
+      return;
+    }
+
+    this.events[when][name] = func;
+  },
+
+  blend: function (name, func) {
+    this.blends[name] = func;
+  },
+
+  shape: function (name, func) {
+    this.shapes[name] = func;
+  },
+
+  colormap: function (name, func) {
+    this.colormaps[name] = func;
+  },
+
+  init: function (width, height, normalize) {
+    return this.getGenerator(width, height, normalize);
+  },
+};
+
+require("./tgen-blends")(SeamlessTextureGenerator);
+require("./tgen-shapes")(SeamlessTextureGenerator);
+require("./tgen-colormaps")(SeamlessTextureGenerator);
+require("./tgen-functions")(SeamlessTextureGenerator);
+require("./tgen-generator")(SeamlessTextureGenerator);
+require("./effects/index")(SeamlessTextureGenerator);
+require("./filters/index")(SeamlessTextureGenerator);
+require("./tgen-tests")(SeamlessTextureGenerator);
+
+module.exports = SeamlessTextureGenerator;
