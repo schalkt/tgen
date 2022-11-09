cf875dd25c076f0cd38529ce1e3477973d91225f
TS <git@schalk.hu>
2022-11-09 17:15:48 +0100

    Updated package files because of loader-utils vulnerability

--- src/tgen-base-common.js
+++ src/tgen-base-common.js
@@ -5,13 +5,13 @@
  * https://texture-generator.com/
  *
  * @copyright 2015-2022 Tamas Schalk
- * @version 1.4.12
+ * @version 1.4.13
  * @license MIT
  *
  */
 
 var SeamlessTextureGenerator = {
-  version : "1.4.12",
+  version : "1.4.13",
   defaults: {},
   effects: {},
   prepare: {},
