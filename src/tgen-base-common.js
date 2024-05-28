5e55d5569a62d7e9c190467b95827212c2e8e723
TS <git@schalk.hu>
2024-05-28 20:28:19 +0200

    Updated package files Updated version

--- src/tgen-base-common.js
+++ src/tgen-base-common.js
@@ -5,13 +5,13 @@
  * https://texture-generator.com/
  *
  * @copyright 2015-2022 Tamas Schalk
- * @version 1.4.25
+ * @version 1.4.26
  * @license MIT
  *
  */
 
 const SeamlessTextureGenerator = {
-  version : "1.4.25",
+  version : "1.4.26",
   defaults: {},
   effects: {},
   prepare: {},
