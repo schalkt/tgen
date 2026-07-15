b79a2bd688e0cac49ebc91c5111a4bee1c667786
TS <git@schalk.hu>
2026-07-15 08:50:47 +0200

    chore: bump version to 1.4.43 and update dependencies
    - Updated version in LICENSE.txt, package.json, and source files to 1.4.43
    - Updated package-lock.json with new dependency versions:
      - @babel/core: 8.0.1
      - @babel/preset-env: 8.0.2
      - eslint: 10.7.0
      - webpack: 5.108.4
      - Various other dependencies updated to their latest versions
    - Regenerated distribution files: dist/tgen.min.js, dist/tgen-with-presets.min.js, and their corresponding gzipped versions

--- src/tgen-base-common.js
+++ src/tgen-base-common.js
@@ -5,13 +5,13 @@
  * https://texture-generator.com/
  *
  * @copyright 2015-2026 Tamas Schalk
- * @version 1.4.42
+ * @version 1.4.43
  * @license MIT
  *
  */
 
 const SeamlessTextureGenerator = {
-  version : "1.4.42",
+  version : "1.4.43",
   defaults: {},
   effects: {},
   prepare: {},
